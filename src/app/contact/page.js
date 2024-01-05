646e2e05d4a3d166a8068baa7197b45048f0b266
Jorge Dagor <dlgcjorge@gmail.com>
2024-01-05 18:23:47 +0100

    Some improvements in the footer.

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,16 +1,56 @@
 'use client';
 
-import { Column, Grid } from '@carbon/react';
+import {
+  Column,
+  Grid,
+  Form,
+  Stack,
+  TextInput,
+  TextArea,
+  Button
+} from '@carbon/react';
+
+const TextInputName = {
+  className: 'form-name-class',
+  id: 'name',
+  placeholder: 'Name'
+};
+
+const TextInputEmail = {
+  className: 'form-email-class',
+  id: 'email',
+  placeholder: 'Email'
+};
+
+const TextAreaMessage = {
+  className: 'form-message-class',
+  id: 'message',
+  placeholder: 'Message',
+  rows: 5
+};
+
+const buttonEvents = {
+  className: 'some-class'
+};
 
 function ContactPage() {
-    return (
-      <Grid className="exams-page">
-        <Column lg={16} md={8} sm={4} className="exams-page__r1">
-            <div>CONTACT PAGE</div>
-        </Column>
-      </Grid>
-    );
-  }
-  
-  export default ContactPage;
+  return (
+    <Grid className="exams-page">
+      <Column lg={16} md={8} sm={4} className="exams-page__r1">
+        <Form aria-label="sample form">
+          <Stack gap={7}>
+            <TextInput {...TextInputName} />
+            <TextInput {...TextInputEmail} />
+            <TextArea {...TextAreaMessage} />
+            <Button type="submit" className="some-class" {...buttonEvents}>
+              Submit
+            </Button>
+          </Stack>
+        </Form>
+      </Column>
+    </Grid>
+  );
+}
+
+export default ContactPage;
 
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,68 +2,54 @@
 
 import {
   Column,
-  Grid,
-  Form,
-  Stack,
-  TextInput,
-  TextArea,
-  Button
+  Grid
 } from '@carbon/react';
-
-const TextInputName = {
-  className: 'form-name-class',
-  id: 'name',
-  placeholder: 'Name'
-};
-
-const TextInputEmail = {
-  className: 'form-email-class',
-  id: 'email',
-  placeholder: 'Email'
-};
-
-const TextAreaMessage = {
-  className: 'form-message-class',
-  id: 'message',
-  placeholder: 'Message',
-  rows: 5
-};
-
-const buttonEvents = {
-  className: 'some-class'
-};
+import Link from 'next/link';
 
 const iframeStyle = {
   width: '100%',  // Puedes ajustar el ancho según tus necesidades
-  height: '400px',  // Puedes ajustar la altura según tus necesidades
+  height: '200px',  // Puedes ajustar la altura según tus necesidades
 };
 
 function Footer() {
   return (
     <Grid className="footer" fullWidth>
-      <Column
-        lg={8}
-        md={4}
-        sm={4}
-        className="footer__r1">
-        <div class="widget-title">Escríbenos</div>
-
-        <Form aria-label="sample form">
-          <Stack gap={7}>
-            <TextInput {...TextInputName} />
-            <TextInput {...TextInputEmail} />
-            <TextArea {...TextAreaMessage} />
-            <Button type="submit" className="some-class" {...buttonEvents}>
-              Submit
-            </Button>
-          </Stack>
-        </Form>
+      <Column lg={16} md={16} sm={16}>
+        <h2 class="widget-title">More info</h2>
       </Column>
       <Column
-        lg={8}
+        lg={4}
         md={4}
         sm={4}
-        className="footer__r2">
+        className="footer__c1">
+        <div>
+
+          <h4>Phone</h4>
+          <Link href="tel:+34910672712" passHref>
+            <p>(+34) 910672712</p>
+          </Link>
+          {<br />}
+
+          <h4>Email</h4>
+          <Link href="mailto:futuroalumno@etsiinf.upm.es" passHref>
+            <p>futuroalumno@etsiinf.upm.es</p>
+          </Link>
+          {<br />}
+
+          <h4>Questions</h4>
+          <p>Have questions? Write us on <Link href="/contact" passHref>AskQuestions</Link>.</p>
+        </div>
+      </Column>
+      <Column
+        lg={12}
+        md={4}
+        sm={4}
+        className="footer__c2">
+          <h4>Address</h4>
+          <Link href="https://www.google.com/maps?ll=40.40501,-3.839519&z=15&t=m&hl=es-ES&gl=US&mapclient=embed&cid=16103465423937228709" passHref>
+            <p>ETS de Ingenieros Informáticos Universidad Politécnica de Madrid Campus de Montegancedo, s/n, Pozuelo de Alarcón(28223), España</p>
+          </Link>
+          {<br />}
         <div>
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3038.171243133389!2d-3.841730548564272!3d40.4050570641712!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd4185f258e5eec5%3A0xdf7b007bb6904fa5!2sUniversidad+Polit%C3%A9cnica+de+Madrid%3A+Escuela+T%C3%A9cnica+Superior+de+Ingenieros+Inform%C3%A1ticos!5e0!3m2!1ses!2ses!4v1465287304393" frameborder="0" allowfullscreen style={iframeStyle}></iframe>
         </div>
