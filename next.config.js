146972f7fd52eccd9321cdd05d450de56f08aa58
Jorge Dagor <dlgcjorge@gmail.com>
2024-01-09 17:46:34 +0100

    Testing GitHub pages.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
+const repo = 'https://jdlgc.github.io/etsiinf-webpage-redesigned'
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
+
 const nextConfig = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
     images: {
         domains: ['www.fi.upm.es'],
       },
 }
 
 module.exports = nextConfig
+
+
