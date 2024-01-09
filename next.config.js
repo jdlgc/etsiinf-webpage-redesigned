5a04689ae1ced9ec036509854f63be8d761e0305
Jorge Dagor <dlgcjorge@gmail.com>
2024-01-09 18:29:35 +0100

    Testing GitHub pages.

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
-const isProd = process.env.NODE_ENV === 'production';
-const assetPrefix = isProd ? '/etsiinf-webpage-redesigned' : '';
-
-module.exports = {
-  basePath: isProd ? '/etsiinf-webpage-redesigned' : '',
-  assetPrefix: assetPrefix,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     domains: ['www.fi.upm.es'],
+    unoptimized: true,
   },
   output: "export"
 };
+
+module.exports = nextConfig;
